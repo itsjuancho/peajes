bfa33dcb79af576a6a74946176577851f371f77d
juancho <juanandresperezpt@gmail.com>
2021-03-19 14:51:23 -0500

    Versión 1.0

---
+++ Peaje/frmPeaje.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Peaje
+{
+    partial class frmPeaje
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nPeaje = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ctrUno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ctrDos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ctrTres = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmVentaTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(73, 291);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 0;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Enabled = false;
+            this.btnCalcular.Location = new System.Drawing.Point(186, 291);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Enabled = false;
+            this.btnReiniciar.Location = new System.Drawing.Point(337, 291);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnReiniciar.TabIndex = 2;
+            this.btnReiniciar.Text = "Reiniciar";
+            this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nPeaje,
+            this.ctrUno,
+            this.ctrDos,
+            this.ctrTres,
+            this.clmVentaTotal});
+            this.dataGridView1.Location = new System.Drawing.Point(28, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(543, 251);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // nPeaje
+            // 
+            this.nPeaje.HeaderText = "Nombre Peaje";
+            this.nPeaje.Name = "nPeaje";
+            this.nPeaje.ReadOnly = true;
+            // 
+            // ctrUno
+            // 
+            this.ctrUno.HeaderText = "Categoría 1";
+            this.ctrUno.Name = "ctrUno";
+            this.ctrUno.ReadOnly = true;
+            // 
+            // ctrDos
+            // 
+            this.ctrDos.HeaderText = "Categoría dos";
+            this.ctrDos.Name = "ctrDos";
+            this.ctrDos.ReadOnly = true;
+            // 
+            // ctrTres
+            // 
+            this.ctrTres.HeaderText = "Categoría tres";
+            this.ctrTres.Name = "ctrTres";
+            this.ctrTres.ReadOnly = true;
+            // 
+            // clmVentaTotal
+            // 
+            this.clmVentaTotal.HeaderText = "Venta Total";
+            this.clmVentaTotal.Name = "clmVentaTotal";
+            this.clmVentaTotal.ReadOnly = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(459, 291);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // frmPeaje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 336);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnIngresar);
+            this.Name = "frmPeaje";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnReiniciar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nPeaje;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ctrUno;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ctrDos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ctrTres;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmVentaTotal;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Peaje/frmPeaje.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Peaje
+{
+    public partial class frmPeaje : Form
+    {
+        int N, F, C;
+        public string[] NombrePeaje;
+        public int[,] MPeajes;
+        public int[,] MResultados;
+        public string[,] MMPPeaje;
+
+        public frmPeaje()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try {
+                N = Convert.ToInt32(Interaction.InputBox("Ingrese el número de peajes"));
+                dataGridView1.RowCount = (N + 4);
+                dataGridView1.Rows[N].Cells[0].Value = "Venta total de categoría";
+                dataGridView1.Rows[N + 1].Cells[0].Value = "Promedio";
+                dataGridView1.Rows[N + 2].Cells[0].Value = "Mejor peaje por Categoría";
+                dataGridView1.Rows[N + 3].Cells[0].Value = "Peor peaje por Categoría";
+                dataGridView1.Rows[N].Cells[4].Style.BackColor = Color.Transparent;
+                dataGridView1.Rows[N + 1].Cells[4].Style.BackColor = Color.Transparent;
+                dataGridView1.Rows[N + 2].Cells[4].Style.BackColor = Color.Transparent;
+                dataGridView1.Rows[N + 3].Cells[4].Style.BackColor = Color.Transparent;
+
+                NombrePeaje = new String[N];
+                MPeajes = new int[N, 4];
+                for(F=0; F < N; F++)
+                {
+                    NombrePeaje[F] = Interaction.InputBox("Ingrese el nombre del peaje");
+                    dataGridView1.Rows[F].Cells[0].Value = NombrePeaje[F];
+
+                    MPeajes[F, 0] = Convert.ToInt32(Interaction.InputBox("Ingrese la cantidad de autos para la categoría 1"));
+                    dataGridView1.Rows[F].Cells[1].Value = MPeajes[F, 0];
+
+                    MPeajes[F, 1] = Convert.ToInt32(Interaction.InputBox("Ingrese la cantidad de autos para la categoría 2"));
+                    dataGridView1.Rows[F].Cells[2].Value = MPeajes[F, 1];
+
+                    MPeajes[F, 2] = Convert.ToInt32(Interaction.InputBox("Ingrese la cantidad de autos para la categoría 3"));
+                    dataGridView1.Rows[F].Cells[3].Value = MPeajes[F, 2];
+
+                    MPeajes[F, 3] = ((MPeajes[F, 0] * 8000) + (MPeajes[F, 1] * 10500) + (MPeajes[F, 2] * 15000));
+                    dataGridView1.Rows[F].Cells[4].Value = string.Format("{0:c}", MPeajes[F, 3]);
+                }
+                btnIngresar.Enabled = false;
+                btnCalcular.Enabled = true;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Ocurrió un error al ejecutar o seguir con el programa, inténtalo de nuevo.");
+                dataGridView1.RowCount = 0;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            MResultados = new int[N, 3];
+            MMPPeaje = new String[N, 3];
+            int Ac = 0;
+            int V = 0;
+            int Aux = 0;
+            string MejorP = "", PeorP = "";
+            for (C=0; C < 3; C++)
+            {
+                for (F=0; F < N; F++)
+                {
+                    Ac = Ac + MPeajes[F, C];
+                    if (C == 0)
+                    {
+                        V = 8000;
+                    }else if (C == 1)
+                    {
+                        V = 10500;
+                    }else if (C == 2)
+                    {
+                        V = 15000;
+                    }
+
+                    if (F == 0)
+                    {
+                        MejorP = NombrePeaje[F];
+                        PeorP = NombrePeaje[F];
+                    }
+                    else
+                    {
+                        if (MPeajes[F, C] > Aux)
+                        {
+                            Aux = MPeajes[F, C];
+                            MejorP = NombrePeaje[F];
+                        }
+                        else
+                        {
+                            Aux = MPeajes[F, C];
+                            PeorP = NombrePeaje[F];
+                        }
+                    }
+                }
+                MResultados[0, C] = Ac * V;
+                dataGridView1.Rows[N].Cells[C+1].Value = string.Format("{0:c}", MResultados[0, C]);
+                MResultados[1, C] = MResultados[0, C] / N;
+                dataGridView1.Rows[N+1].Cells[C+1].Value = string.Format("{0:c}", MResultados[1, C]);
+                MMPPeaje[0, C] = MejorP;
+                dataGridView1.Rows[N + 2].Cells[C + 1].Value = MMPPeaje[0, C];
+                MMPPeaje[1, C] = PeorP;
+                dataGridView1.Rows[N + 3].Cells[C + 1].Value = MMPPeaje[1, C];
+            }
+            btnCalcular.Enabled = false;
+            btnReiniciar.Enabled = true;
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            btnIngresar.Enabled = true;
+            btnCalcular.Enabled = false;
+            btnReiniciar.Enabled = false;
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult Result = MessageBox.Show("¿Está seguro que va a salir del aplicativo?", "Salir App", MessageBoxButtons.YesNo);
+            if(Result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                MessageBox.Show("Continuará la ejecución...");
+            }
+        }
+    }
+}
